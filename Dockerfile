b7eed0bc5854f247dcab06d35962cbec57aba429
Abdul Hafeez <abdulhafeez.mashori@tajawal.com>
2020-04-30 06:54:04 +0400

    added wait for tool to wait for container untill it is fully initialized

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
 FROM openjdk:8-jdk-alpine
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} poc.jar
+
+# Add docker-compose-wait tool -------------------
+ENV WAIT_VERSION 2.7.2
+ADD https://github.com/ufoscout/docker-compose-wait/releases/download/$WAIT_VERSION/wait /wait
+RUN chmod +x /wait
+
 ENTRYPOINT ["java","-jar","/poc.jar"]
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
+git clone https://github.com/hafeeztsd/poc.git
+cd poc/
 docker network create app-mysql_network
-docker container run --name mysqlhost --network app-mysql_network -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=task_management -d mysql:5.7
 mvn clean install
-docker image build -t app-poc .
-docker-compose up 
+docker-compose up
